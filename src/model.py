2b9fa314017494743a053f1cab6132e1e3f38491
enceladus-rex <49532843+enceladus-rex@users.noreply.github.com>
2019-07-06 22:51:56 -0400

    Pretest model complete

--- src/model.py
+++ src/model.py
@@ -6,15 +6,32 @@ from chainer import Link, Chain
 import chainer.functions as F
 import chainer.links as L
 
+import cupy as cp
+
 
 HeadData = namedtuple('HeadData', ('query', 'key', 'value'))
 
 
-def scaled_dot_product_attention(queries, keys, values):
+def scaled_dot_product_attention(queries, keys, values, mask=None):
   x1 = F.matmul(queries, keys, transb=True) / self.scale
-  x2 = F.softmax(x1)
-  x3 = F.matmul(x2, values)
-  return x3
+  x2 = x1 * mask if mask is not None else x1 
+  x3 = F.softmax(x2)
+  x4 = F.matmul(x3, values)
+  return x4
+
+
+def generate_positional_encoding(start, end, dim):
+  positions = cp.arange(start, end)
+  stacks = []
+  for i in range(dim):
+    divisor = 10000 ** (2 * i / float(dim))
+    elements = positions / divisor
+    if i % 2 == 0:
+      pe = cp.sin(elements)
+    else:
+      pe = cp.cos(elements)
+    stacks.append(pe)
+  return cp.transpose(cp.stack(stacks))
 
 
 class MultiHeadAttention(Chain):
@@ -35,14 +52,14 @@ class MultiHeadAttention(Chain):
             L.Linear(model_dim, value_dim)))
       self.output_link = L.Linear(self.multi_head_dim, model_dim)
 
-  def forward(self, queries, keys, values):
+  def forward(self, queries, keys, values, mask=None):
     projections = []
     heads = []
     for i in range(self.num_heads):
       hl = self.head_links[i]
 
       projection = HeadData(hl.query(queries), hl.key(keys), hl.value(values))
-      head = scaled_dot_product_attention(*projection)
+      head = scaled_dot_product_attention(*projection, mask=mask)
 
       projections.append(projection)
       heads.append(head)
@@ -63,7 +80,8 @@ class PointwiseFeedForwardNetwork(Chain):
 
 
 class TransformerEncoderUnit(Chain):
-  def __init__(self, num_heads, model_dim, ff_dim):
+  def __init__(self, num_heads, model_dim, ff_dim, p_drop):
+    self.p_drop = p_drop
     with self.init_scope():
       kv_dim = model_dim // num_heads
       self.mha = MultiHeadAttention(num_heads, model_dim, kv_dim, kv_dim)
@@ -72,19 +90,21 @@ class TransformerEncoderUnit(Chain):
       self.ff = PointwiseFeedForwardNetwork(model_dim, ff_dim)
   
   def forward(self, inputs_unit):
-    x1 = self.mha(inputs_unit, inputs_unit, inputs_unit)
+    x1 = F.dropout(
+      self.mha(inputs_unit, inputs_unit, inputs_unit), self.p_drop)
     x2 = self.lnorm1(inputs_unit + x1)
-    x3 = self.ff(x2)
+    x3 = F.dropout(self.ff(x2), self.p_drop)
     x4 = self.lnorm1(x2 + x3)
     return x4
 
 
 class TransformerDecoderUnit(Chain):
-  def __init__(self, num_heads, model_dim, ff_dim):
+  def __init__(self, num_heads, model_dim, ff_dim, p_drop):
     super().__init__()
+    self.p_drop = p_drop
     with self.init_scope():
       kv_dim = model_dim // num_heads
-      self.mmha = MaskedMultiHeadAttention(
+      self.mmha = MultiHeadAttention(
         num_heads, model_dim, kv_dim, kv_dim)
       self.mha = MultiHeadAttention(
         num_heads, model_dim, kv_dim, kv_dim)
@@ -94,24 +114,29 @@ class TransformerDecoderUnit(Chain):
       self.ff = PointwiseFeedForwardNetwork(model_dim, ff_dim)
   
   def forward(self, inputs_encoding, outputs_unit):
-    x1 = self.mmha(outputs_unit, outputs_unit, outputs_unit)
+    mask_shape = tuple(inputs_encoding.shape)
+    mask_shape[-1] = mask_shape[-2]
+    mask = cp.tril(cp.ones(mask_shape), inputs_encoding.dtype)
+    x1 = F.dropout(
+      self.mmha(outputs_unit, outputs_unit, outputs_unit, mask), self.p_drop)
     x2 = self.lnorm1(outputs_unit + x1)
-    x3 = self.mha(inputs_encoding, inputs_encoding, x2)
+    x3 = F.dropout(
+      self.mha(inputs_encoding, inputs_encoding, x2), self.p_drop)
     x4 = self.lnorm2(x2 + x3)
-    x5 = self.ff(x4)  # TODO: Determine if an activation is required with this.
+    x5 = F.dropout(self.ff(x4), self.p_drop)
     x6 = self.lnorm3(x4 + x5)
     return x6
 
 
 class TransformerEncoder(Chain):
 
-  def __init__(self, depth, num_heads, model_dim, ff_dim):
+  def __init__(self, depth, num_heads, model_dim, ff_dim, p_drop):
     super().__init__()
     self.unit_links = []
     with self.init_scope():
       for i in range(depth):
         self.unit_links.append(
-          TransformerEncoderUnit(num_heads, model_dim, ff_dim))
+          TransformerEncoderUnit(num_heads, model_dim, ff_dim, p_drop))
         
   def forward(self, inputs_encoding):
     unit_inputs = [inputs_encoding]
@@ -122,20 +147,15 @@ class TransformerEncoder(Chain):
     return unit_inputs[-1]
 
 
-class MaskedMultiHeadAttention(Chain):
-  def __init__(self):
-    pass
-
-
 class TransformerDecoder(Chain):
-  def __init__(self, depth, num_heads, model_dim, ff_dim):
+  def __init__(self, depth, num_heads, model_dim, ff_dim, p_drop):
     super().__init__()
     self.unit_links = []
     with self.init_scope():
       self.lin1 = L.Linear(model_dim)
       for i in range(depth):
         self.unit_links.append(
-          TransformerDecoderUnit(num_heads, model_dim, ff_dim))
+          TransformerDecoderUnit(num_heads, model_dim, ff_dim, p_drop))
 
   def forward(self, inputs_encoding, outputs_unit):
     unit_inputs = [outputs_unit]
@@ -148,18 +168,51 @@ class TransformerDecoder(Chain):
 
 
 class Transformer(Chain):
-  def __init__(self, depth=6, num_heads=8, model_dim=512, ff_dim=2048):
+  def __init__(
+      self, model_dim=512, num_heads=8, encoder_depth=6, 
+      decoder_depth=6, ff_dim=2048, p_drop=0.1):
     super().__init__()
-    self.depth = depth
+    self.encoder_depth = encoder_depth
+    self.decoder_depth = decoder_depth
     self.num_heads = num_heads
     self.model_dim = model_dim
     self.ff_dim = ff_dim
+    self.p_drop = p_drop
+    self.multiplier = sqrt(model_dim)
     with self.init_scope():
-      self.encoder = TransformerEncoder(depth, num_heads, model_dim, ff_dim)
-      self.decoder = TransformerDecoder(depth, num_heads, model_dim, ff_dim)
+      self.encoder = TransformerEncoder(
+        encoder_depth, num_heads, model_dim, ff_dim, p_drop)
+      self.decoder = TransformerDecoder(
+        decoder_depth, num_heads, model_dim, ff_dim, p_drop)
+      self.linear_embedding = L.Linear(None, model_dim, nobias=True)
   
-  def forward(self, inputs, shifted_outputs):
-    # TODO: Generate input embedding.
-    # TODO: Add positional encoding.
-    inputs_encoding = self.encoder(inputs)
-    return self.decoder(inputs_encoding, shifted_outputs)
+  def forward(
+      self, inputs, outputs, inputs_position, outputs_position):
+    assert inputs.shape == outputs.shape
+
+    inputs_positional_encoding = generate_positional_encoding(
+      inputs_position, 
+      inputs_position + inputs.shape[-2], 
+      inputs.shape[-1])
+    
+    outputs_positional_encoding = generate_positional_encoding(
+      outputs_position, 
+      outputs_position + outputs.shape[-2], 
+      outputs.shape[-1])
+
+    input_embedding = self.linear_embedding(inputs) * self.multiplier
+    output_embedding = self.linear_embedding(shifted_outputs) * self.multiplier
+    
+    transformed_inputs = F.dropout(
+      inputs_positional_encoding + input_embedding, self.p_drop)
+    transformed_ouputs = F.dropout(
+      outputs_positional_encoding + output_embedding, self.p_drop)
+
+    encoding = self.encoder(transformed_inputs)
+    decoding = self.decoder(encoding, transformed_ouputs)
+
+    inverse_embedding = F.linear(
+      decoding, F.transpose(self.linear_embedding.W))
+    next_token_probabilities = F.softmax(inverse_embedding)
+
+    return next_token_probabilities
